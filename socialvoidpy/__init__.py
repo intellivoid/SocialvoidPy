f14abfb69f1a164a101e770fd0fa722e6d821e7d
blank X <theblankx@protonmail.com>
2021-09-15 21:40:43 +0700

    Rename sync to sync_client

--- socialvoidpy/__init__.py
+++ socialvoidpy/__init__.py
@@ -1,5 +1,5 @@
 from .errors import *
 from . import types
-from .sync import SocialvoidClient
+from .sync_client import SocialvoidClient
 from .async_client import AsyncSocialvoidClient
 from .version import version as __version__
