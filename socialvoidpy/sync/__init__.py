59019277e65de7d3087d1cc7343b4e0aa98e42f3
blank X <theblankx@protonmail.com>
2021-09-15 16:55:40 +0700

    Replace requests with httpx

--- socialvoidpy/sync/__init__.py
+++ socialvoidpy/sync/__init__.py
@@ -1,5 +1,5 @@
 import secrets
-import requests
+import httpx
 from ..utils import parse_jsonrpc_response, serialize_request
 from .session import Session
 from .help import Help
@@ -8,11 +8,11 @@ from .account import Account
 from .cloud import Cloud
 
 class SocialvoidClient:
-    def __init__(self, filename=None, rpc_endpoint='http://socialvoid.qlg1.com:5601/', http_session=None):
+    def __init__(self, filename=None, rpc_endpoint='http://socialvoid.qlg1.com:5601/', httpx_client=None):
         self.rpc_endpoint = rpc_endpoint
-        if http_session is None:
-            http_session = requests.Session()
-        self.http_session = http_session
+        if httpx_client is None:
+            httpx_client = httpx.Client()
+        self.httpx_client = httpx_client
         if filename is None:
             self.session = Session(self)
         else:
@@ -26,6 +26,12 @@ class SocialvoidClient:
         self.account = Account(self)
         self.cloud = Cloud(self)
 
+    def close(self):
+        self.httpx_client.close()
+
+    def __del__(self):
+        self.close()
+
     def _save_session(self):
         if self._session_filename is not None:
             self.session.save(self._session_filename)
@@ -37,5 +43,5 @@ class SocialvoidClient:
         if not batch:
             requests = requests[0]
         body = serialize_request(requests)
-        resp = self.http_session.post(self.rpc_endpoint, json=body)
+        resp = self.httpx_client.post(self.rpc_endpoint, json=body)
         return parse_jsonrpc_response(resp.text, batch)
