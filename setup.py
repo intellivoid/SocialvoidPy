638a970660fe91589331f640fdec47a5f193f484
blank X <blankie@nixnetmail.com>
2021-10-04 14:13:10 +0700

    Add {python,install}_requires to setup.py

--- setup.py
+++ setup.py
@@ -7,11 +7,14 @@ with open("README.md") as file:
 setup(
     name="socialvoidpy",
     version=socialvoidpy.__version__,
+    description="Official socialvoid python library",
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="blank X",
     author_email="blankie@nixnetmail.com",
     url="https://github.com/Intellivoid/SocialvoidPy",
+    python_requires=">=3.6",
     include_package_data=True,
     packages=find_packages(exclude=["examples"]),
+    install_requires=["httpx==0.19.*"],
 )
