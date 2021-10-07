6d2a0218825203448158f90d8cc84271a64e968c
blank X <blankie@nixnetmail.com>
2021-10-07 20:21:05 +0700

    Handle created_timestamp in Document being removed

--- socialvoidpy/types/document.py
+++ socialvoidpy/types/document.py
@@ -36,7 +36,7 @@ class Document(BaseClass):
     - **file_size**: The file size in bytes
     - **file_type**: A [`FileType`](#filetype)
     - **flags**: Flags set for the document
-    - **created**: When the document was created
+    - **created**: When the document was created (removed in 1.0.0.1)
     """
 
     id: str
@@ -45,10 +45,14 @@ class Document(BaseClass):
     file_size: int
     file_type: FileType
     flags: typing.List[str]
-    created: datetime.datetime
+    created: typing.Optional[datetime.datetime]
 
     @classmethod
     def from_json(cls, resp: dict):
+        if "created_timestamp" in resp:
+            created = datetime.datetime.fromtimestamp(resp["created_timestamp"])
+        else:
+            created = None
         return cls(
             resp["id"],
             resp["file_mime"],
@@ -56,5 +60,5 @@ class Document(BaseClass):
             resp["file_size"],
             FileType(resp["file_type"]),
             resp["flags"],
-            datetime.datetime.fromtimestamp(resp["created_timestamp"]),
+            created,
         )
