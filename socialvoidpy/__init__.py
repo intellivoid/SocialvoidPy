bfe9b0de102c56cbde7a70430a78e5f4161bde35
blank X <blankie@nixnetmail.com>
2021-10-08 16:05:36 +0700

    Make errors import usage consistent with types

--- socialvoidpy/__init__.py
+++ socialvoidpy/__init__.py
@@ -1,5 +1,4 @@
-from .errors import *
-from . import types
+from . import types, errors
 from .sync_client import SocialvoidClient
 from .async_client import AsyncSocialvoidClient
 from .version import version as __version__
